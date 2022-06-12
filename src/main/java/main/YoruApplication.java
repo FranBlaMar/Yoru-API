07bf48e8c3d21245398eae7ab39c1925ddb1b196
FranBlaMar <74529306+FranBlaMar@users.noreply.github.com>
2022-06-12 16:31:16 +0200

    Update YoruApplication.java

--- src/main/java/main/YoruApplication.java
+++ src/main/java/main/YoruApplication.java
@@ -13,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import main.model.Hobbie;
 import main.model.User;
 import main.repository.HobbieRepository;
-import main.repository.UserRepository;
 
 @SpringBootApplication
 public class YoruApplication {
@@ -26,21 +25,8 @@ public class YoruApplication {
 	}
 	
 	@Bean
-	CommandLineRunner initData(UserRepository userRepository, HobbieRepository hobbieRepository) {
+	CommandLineRunner initData(HobbieRepository hobbieRepository) {
 		return args -> {
-			
-			User us1 = new User("Yoru",encoder.encode("123"), "franbl98@gmail.com", "", null, "ADMIN");
-			us1.setNumeroPublicaciones(0);
-			us1.setNumeroSeguidores(0);
-			us1.setNumeroSeguidos(0);
-			us1.setAboutMe("Cuenta oficila de Yoru. Conecta con tus amigos.");
-			User us2 = new User("David",encoder.encode("123"), "franbl984@gmail.com", "", null, "USER");
-			us2.setNumeroPublicaciones(0);
-			us2.setNumeroSeguidores(0);
-			us2.setNumeroSeguidos(0);
-			us2.setAboutMe("cuenta personal de David, encantado!");
-			userRepository.save(us1);
-			userRepository.save(us2);
 			
 			Hobbie h1 = new Hobbie("DEPORTES");
 			Hobbie h2 = new Hobbie("MUSICA");
